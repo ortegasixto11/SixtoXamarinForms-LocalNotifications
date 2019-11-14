3bfac58a612f284d9feae9da05c170b1c2a6a1c2
Sixto Ortega <sixto_miguel@hotmail.com>
2019-11-14 09:47:37 -0400

    Upload project

---
+++ LocalNotifications/LocalNotifications/LocalNotifications/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Plugin.LocalNotifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LocalNotifications
+{
+    /*
+     * Link del Articulo 
+     * https://xamarinlatino.com/local-notifications-en-xamarin-forms-4d213092d0c
+     * 
+     * Link del Repo
+     * https://github.com/codercampos/XF-localnotifications
+     * 
+     * Link del nuget
+     * https://www.nuget.org/packages/Xam.Plugins.Notifier
+     * 
+     */
+
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButton_Clicked(object sender, EventArgs e)
+        {
+            CrossLocalNotifications.Current.Show("Xamarin App", "Es una notificación", DateTime.Now.Millisecond, DateTime.Now.AddSeconds(2));
+        }
+    }
+}
